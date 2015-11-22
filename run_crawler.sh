8c451f72a5d50048ef902823d9d0974a34c62650
Osama Sakhi <msakhi21@gmail.com>
2015-11-21 22:27:09 -0500

    fixed run script

--- run_crawler.sh
+++ run_crawler.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -eu
 
 # TODO: Move this date stamp to Python.
+TO_HERE="${BASH_SOURCE[0]}";
+THIS_FILE=$(basename $TO_HERE)
+
+TO_THIS_DIR=${TO_HERE%$THIS_FILE}
+
+echo $TO_THIS_DIR;
+
+cd $TO_THIS_DIR
+
 echo $(date) >> crawler_runs.log
 python crawler.py >> crawler_runs.log
+
